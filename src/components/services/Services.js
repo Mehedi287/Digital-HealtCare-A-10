a54750586c4af5a9d0e2d24d531f4f6abbb7aa49
Mehedi390 <mh0816150@gmail.com>
2021-10-18 08:24:41 -0700

    add not found page

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import "./Services.css"
 const Services = (props) => {
 
     const { name, id, img, details, price } = props.service;
     console.log(props);
     return (
         <>
-            <div>
-                <h2>name {name}</h2>
-                <img src={img} alt="" />
+            <div className="service">
+                <h4>Name : {name}</h4>
+                <img className="rounded" src={img} alt="" />
                 <p>{details}</p>
-                <p>{price}</p>
-                <Link to={`/details/${id}`}><button>Details</button></Link>
+                <p>Price : {price}</p>
+                <Link className="custom-btn" to={`/details/${id}`}><button class="button-82-pushable" role="button">
+                    <span class="button-82-shadow"></span>
+                    <span class="button-82-edge"></span>
+                    <span class="button-82-front text">
+                        Details
+                    </span>
+                </button></Link>
             </div>
 
         </>
